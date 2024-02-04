86905753f4ccbf873ac9fea202f2c042b1d7600c
Edisson Torres <87282774+eddi-dev95@users.noreply.github.com>
2024-02-04 00:08:03 -0500

    Actualizar script.js
    Example for \n & \t

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onload = () => {
       // Copiar al portapapeles (con comprobación de compatibilidad)
       if (navigator.clipboard && navigator.clipboard.writeText) {
         navigator.clipboard.writeText(inputApi.value)
-          .then(() => alert("¡Bien hecho!"));
+          .then(() => alert("\n\t\t¡Bien hecho!"));
           window.close();
           .catch(err => alert("Error al copiar: " + err));
       } else {
